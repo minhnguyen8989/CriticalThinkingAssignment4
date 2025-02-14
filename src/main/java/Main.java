7327466521f32f3dffff12c4941ecd8f23affbed
Minh Nguyen <122935530+minhnguyen8989@users.noreply.github.com>
2025-02-14 13:21:13 -0600

    completed assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //Days and Temperatures array list
         String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
         ArrayList<Double> tempF = new ArrayList<>();
 
+        //Declare Variables
         double tempTotal = 0;
+        double weeklyAverage = 0;
 
+        //Prompt user to input temperatures °F loop structures
         for(int i = 0; i<days.length; i++){
             System.out.print("Enter the temperature °F in " + days[i].toUpperCase() + ": ");
             double tempInput = sc.nextDouble();
@@ -18,8 +21,10 @@ public class Main {
             tempTotal += tempInput;
         }
 
-        double weeklyAverage = tempTotal/days.length;
+        //weeklyAverage Calculation
+        weeklyAverage = tempTotal/days.length;
 
+        //Print each day temperature °F and average temperature in week with loop structures
         for (int i = 0; i < days.length; i++) {
             System.out.println(days[i].toUpperCase() + " temperature is: " + tempF.get(i));
         }
